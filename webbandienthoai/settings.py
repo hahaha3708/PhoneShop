a7065eace8c598ba5423eb44f962a08cb5c7364c
hahaha3708 <minhduc3708@gmail.com>
2025-10-15 21:55:59 +0700

    chạy thành công

--- webbandienthoai/settings.py
+++ webbandienthoai/settings.py
@@ -86,7 +86,7 @@ DATABASES = {
         'USER': 'root',
         'PASSWORD': '123456789',
         'HOST': '127.0.0.1',
-        'PORT': '3307',
+        'PORT': '3306',
         'OPTIONS': {
             'charset': 'utf8mb4',
         },
