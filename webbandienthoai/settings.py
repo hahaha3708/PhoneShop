6ba18ca5368ff853e8bc48a420b4539519248628
Trần Đắc Đại Việt <102798747+vietTNT@users.noreply.github.com>
2025-11-12 21:45:00 +0700

    Revert "sửa lỗi và thêm các chức năng"

--- webbandienthoai/settings.py
+++ webbandienthoai/settings.py
@@ -84,9 +84,9 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'webbandienthoai',
         'USER': 'root',
-        'PASSWORD': '123456789',
+        'PASSWORD': '',
         'HOST': '127.0.0.1',
-        'PORT': '3306',
+        'PORT': '3307',
         'OPTIONS': {
             'charset': 'utf8mb4',
         },
