e265e4e64d5c6b927768cc3126a93e895f693893
hahaha3708 <minhduc3708@gmail.com>
2025-11-18 09:37:06 +0700

    chạy chương trình thành công

--- webbandienthoai/settings.py
+++ webbandienthoai/settings.py
@@ -84,9 +84,9 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'webbandienthoai',
         'USER': 'root',
-        'PASSWORD': '',
+        'PASSWORD': '123456789',
         'HOST': '127.0.0.1',
-        'PORT': '3307',
+        'PORT': '3306',
         'OPTIONS': {
             'charset': 'utf8mb4',
         },
